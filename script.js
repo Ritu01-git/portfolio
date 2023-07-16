1abab739042609e6208964c2cad7bb5c31b2dcfa
ritumahato <ritumahato592002@gmail.com>
2023-07-16 22:53:59 +0530

    portfolio

--- script.js
+++ script.js
@@ -2,22 +2,10 @@ console.log('Its working')
 
 let theme = localStorage.getItem('theme')
 
-if(theme == null){
-	setTheme('light')
-}else{
-	setTheme(theme)
-}
 
-let themeDots = document.getElementsByClassName('theme-dot')
+setTheme('blue')
 
 
-for (var i=0; themeDots.length > i; i++){
-	themeDots[i].addEventListener('click', function(){
-		let mode = this.dataset.mode
-		console.log('Option clicked:', mode)
-		setTheme(mode)
-	})
-}
 
 function setTheme(mode){
 	if(mode == 'light'){
@@ -37,4 +25,72 @@ function setTheme(mode){
 	}
 
 	localStorage.setItem('theme', mode)
+}
+
+var TxtType = function(el, toRotate, period) {
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 10) || 2000;
+	this.txt = '';
+	this.tick();
+	this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
+
+	if (this.isDeleting) {
+	this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+	this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
+
+	this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+	var that = this;
+	var delta = 200 - Math.random() * 100;
+
+	if (this.isDeleting) { delta /= 2; }
+
+	if (!this.isDeleting && this.txt === fullTxt) {
+	delta = this.period;
+	this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === '') {
+	this.isDeleting = false;
+	this.loopNum++;
+	delta = 500;
+	}
+
+	setTimeout(function() {
+	that.tick();
+	}, delta);
+};
+
+window.onload = function() {
+	var elements = document.getElementsByClassName('typewrite');
+	for (var i=0; i<elements.length; i++) {
+		var toRotate = elements[i].getAttribute('data-type');
+		var period = elements[i].getAttribute('data-period');
+		if (toRotate) {
+		  new TxtType(elements[i], JSON.parse(toRotate), period);
+		}
+	}
+	// INJECT CSS
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+	document.body.appendChild(css);
+};
+
+function openGithub(){
+	window.open("https://github.com/Ritu01-git", "_blank");
+}
+
+function openLinkedIn(){
+	window.open("https://github.com/Ritu01-git", "_blank");
+}
+function openResume(){
+	window.open("https://drive.google.com/file/d/16AQbO6Fx9ddTdBO9cm3LLm_izZYE8FBk/view?usp=drive_link", "_blank");
 }
